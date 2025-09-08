598f37ebc211508318fae910d6fd5928d992239e
Leslie <lesliereyesv@gmail.com>
2025-09-08 19:58:09 -0300

    init project

---
+++ src/main/java/com/mycompany/prueba1/Prueba1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prueba1;
+
+/**
+ *
+ * @author Santo Tomas
+ */
+public class Prueba1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        String mensaje = "Este es un mensaje";
+        System.out.println(mensaje);
+    }
+}
