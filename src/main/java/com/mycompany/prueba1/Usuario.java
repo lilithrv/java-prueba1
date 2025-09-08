0494d8efe36822fb97f4616f9763ca58c2d06b39
Leslie <lesliereyesv@gmail.com>
2025-09-08 20:16:59 -0300

    add class: Usuario

---
+++ src/main/java/com/mycompany/prueba1/Usuario.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.prueba1;
+
+/**
+ *
+ * @author Santo Tomas
+ */
+public class Usuario {
+    private String nombre;
+    private String apellido;
+    private byte edad;
+}
